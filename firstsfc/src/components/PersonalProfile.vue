7e2e3969de09eed670f4e81d8e26449296824a1b
Mj Aragon <mdaragon@student.apc.edu.ph>
2026-01-19 19:14:47 +0800

    added profile

--- firstsfc/src/components/PersonalProfile.vue
+++ firstsfc/src/components/PersonalProfile.vue
@@ -1,7 +1,138 @@
 <template>
   <div class="profile">
-    <h1>Introduction page</h1>
-    <p>This is a test message</p>
+    <img src="images/arro.png" id="arro" alt="down arrow">
+<div class="container" id="hite">
+  <div class="container-fluid mt-3">
+    <div class="container faade">
+      <h1>Personal profile page</h1>
+      <h2>About me</h2>
+      <p id="para">
+      I'm a college student from Las Pinas City. I'm the eldest of 5.
+      I like listening to music and traveling. I also like playing games
+      if I don't have energy and want to do something light. I am timid and quiet,
+      but I try my best to be useful in team tasks
+
+      </p>
+    </div>
+    <div class="container-fluid faade">
+      <div class="row">
+        <div class="col-md">
+          <h3>Education and Achievements</h3>
+          <div class="container-fluid">
+            <div class="row" id="bigpara">
+              <div class="col-md">
+                <h4>Course:</h4>
+                <p>BS Information technology</p>
+              </div>
+              <div class="col-md">
+                <h4>Current year:</h4>
+                <p>2nd year college</p>
+              </div>
+            </div>
+            <div class="row">
+              <h4>IT Experience</h4>
+              <p>
+                I have experience when it comes to working with HTML, CSS,
+                a bit of JavaScript, and Python. I also have a little experience
+                using Flutter, since it's being taught. 
+
+              </p>
+            </div>
+          </div>
+
+          
+        </div>
+      </div>  
+    </div>
+    <div class="container-fluid hobs faade">
+      <h3>Interests and Hobbies</h3>
+      <div class="row">
+        <div class="col-md">
+          <h4>Photography</h4>
+          <p>
+            I like taking pictures of sights I see when I'm traveling.
+            I also like taking photos of scenes I set up. 
+          </p>
+
+        </div>
+        <div class="col-md">
+          <h4>Traveling | Roaming </h4>
+          <p>
+            I have a bit of wanderlust, so I find going out to different places
+            and roaming different places enjoyable. This enjoyment of mine also
+            lets me do a bit of photography too
+
+
+          </p>
+
+        </div>
+
+        <div class="col-md">
+          <h4>Gaming</h4>
+          <p>
+            Although I don't play games as intensely as before, I still find enjoyment in
+            playing a game. I especially like Pokemon
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="container-fluid hobs faade">
+
+      <h3>Goals and dreams in life</h3>
+      <div class="row">
+        <div class="col-md">
+          <h4>Goals</h4>
+          <p>
+            My only goal currently is to finish college and 
+            get a job, start earning for money
+          </p>
+        </div>
+        <div class="col-md">
+          <h4>Dreams</h4>
+          <p>
+            I don't have any dreams in life
+          </p>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="hobs container-fluid faade">
+      <h1>Photo Gallery</h1>
+      <div class="row">
+        <div class="col-md">
+          <img src="images/gold.jpg" alt="Photo of BGC covered in light, making it look golden">
+        </div>
+
+        <div class="col-md">
+          <img src="images/irl.jpg" alt="Me in real life">
+        </div>
+
+        <div class="col-md">
+          <img src="images/vibe.jpg" alt="An accidental cool view I got while resting">
+        </div>
+
+        <div class="col-md">
+          <img src="images/me.jpg" alt="My OC">
+        </div>
+
+        <div class="col-md">
+          <img src="images/vrc.png" alt="Relaxing in VRC">
+        </div>
+        
+
+        
+      </div>
+    </div>
+
+    <div class="hobs container-fluid faade">
+      <a href="resources.html" id="resource"><h2>Resources</h2></a>
+    </div>
+  </div>
+</div>
+
+<script src="js/javas.js"></script>
   </div>
 </template>
 
